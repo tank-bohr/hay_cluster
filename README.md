cf2cf964d74f75f5400b3ed9f98066d0df49bb0f
Alexey Nikitin <tank@bohr.su>
2024-07-10 19:42:01 +0400

    feat: release

--- README.md
+++ README.md
@@ -1,3 +1,37 @@
 [![Elixir CI](https://github.com/tank-bohr/hay_cluster/actions/workflows/elixir.yml/badge.svg)](https://github.com/tank-bohr/hay_cluster/actions/workflows/elixir.yml)
 
 # HayCluster
+
+A tool for testing your code in the clustered environment. Focused on correct coverage counting.
+
+Highly inspired by [LocalCluster](https://github.com/whitfin/local-cluster).
+
+## Usage
+
+- Add hay_cluster to your list of dependencies in mix.exs:
+
+```elixir
+defp deps do
+  [
+    {:hay_cluster, "~> 0.1.0"}
+  ]
+end
+```
+
+- Update test_helper.exs
+
+```elixir
+HayCluster.start_distribution()
+Application.ensure_all_started(:my_app)
+ExUnit.start()
+```
+
+- Introduce alias to run test without starting an application:
+
+```elixir
+defp aliases do
+  [
+    test: "test --no-start"
+  ]
+end
+```
--- lib/hay_cluster.ex
+++ lib/hay_cluster.ex
@@ -1,14 +1,28 @@
 defmodule HayCluster do
-  @moduledoc false
+  @moduledoc """
+  """
 
   alias HayCluster.Server
 
+  @doc """
+  Starts the current node as a distributed node with the name of `:"manager@127.0.0.1"`
+  """
   @spec start_distribution() :: {:ok, pid()}
   def start_distribution(), do: :net_kernel.start([:"manager@127.0.0.1"])
 
-  @spec start_nodes(String.t(), pos_integer(), Keyword.t()) :: [atom()]
+  @doc """
+  Starts multiple child nodes and return node names for further use
+  """
+  @spec start_nodes(atom(), pos_integer(), Keyword.t()) :: [atom()]
   def start_nodes(prefix, amount, opts \\ []), do: Server.start_nodes(prefix, amount, opts)
 
+  @doc """
+  Stops the nodes
+  """
   @spec stop_nodes([atom()]) :: :ok
-  def stop_nodes(nodes), do: Server.stop_nodes(nodes)
+  def stop_nodes(nodes) do
+    nodes
+    |> List.wrap()
+    |> Server.stop_nodes()
+  end
 end
